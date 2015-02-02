87d68fa4581c2ec4023e3ea6b3a6091baea29735
Pho Diep <phodiep@gmail.com>
2015-02-02 13:18:39 -0800

    init commit

---
+++ LocationMinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationMinder
+//
+//  Created by Pho Diep on 2/2/15.
+//  Copyright (c) 2015 Pho Diep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
